674bdffc9c74ac49d9c90d725538139a21f55ca5
moeka kitamura <moeka.k76@gmail.com>
2022-01-23 18:27:21 +0900

    use gem ridgepole

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  email: "admin@g.com",
+  password: "111111",
+  admin: true,
+)
+
+5.times do |n|
+  Blog.create!(
+    title:"ブログ#{n}",
+    content:"わたしはドーナツを#{n}つ食べている🍩"
+  )
+end
